138da2d013f750c0940d13676121ca24b756d8fe
Roman Kirillov <sigizmund@gmail.com>
2021-03-09 17:05:18 +0000

    Integrated into main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,10 @@ use indicatif::{ProgressBar, ProgressStyle};
 use log::info;
 use log::LevelFilter;
 
-mod configurator;
+mod config;
 
-use configurator::configurator::Config;
+use config::configurator::Config;
+use getopts::Occur::Optional;
 
 mod error_messages {
     pub const BOTH_MUST_BE_PROVIDED: &str = "Both --src and --dest must be provided";
@@ -36,22 +37,19 @@ struct Photo {
     new_path: Option<String>,
 }
 
-fn main() {
-    use std::env;
-    let args: Vec<String> = env::args().collect();
-    info!("{:?}", args);
-    let action = parse(&args);
-    match action {
-        Ok(action) => match action {
-            Action::CONVERT(config) => {
-                info!("Starting conversion for config {:?}", config);
-                ffmpeg::init().unwrap();
-                convert_files(&config);
-            }
-            _ => {}
-        },
-        Err(_) => {}
+fn main() -> Result<(), Box<std::error::Error>>{
+    let config = config::configurator::get_config(Option::None);
+    if config.is_err() {
+        return Err(config.err().unwrap());
     }
+
+    let config = config.unwrap();
+    info!("Starting conversion for config {:?}", config);
+
+    ffmpeg::init().unwrap();
+    convert_files(&config);
+
+    return Ok(());
 }
 
 fn convert_files(config: &Config) {
@@ -364,86 +362,6 @@ fn move_photo(photo: &Photo, move_file: bool, dry_run: bool) -> Result<(), Error
         }
         Ok(())
     };
-}
-
-fn parse(input: &Vec<String>) -> Result<Action, ArgsError> {
-    let mut args = Args::new("ProgramName", "ProgDesc");
-    args.flag("h", "help", "Print the usage menu");
-    args.flag("t", "dry_run", "Dry-run, do not make actual changes");
-    args.flag("c", "copy", "Copy files instead of moving them");
-    args.option(
-        "s",
-        "src",
-        "Source directory with photos",
-        "SOURCE",
-        Occur::Optional,
-        None,
-    );
-
-    args.option(
-        "d",
-        "dest",
-        "Destination directory",
-        "DEST",
-        Occur::Optional,
-        None,
-    );
-
-    args.option(
-        "l",
-        "log_file",
-        "The name of the log file",
-        "NAME",
-        Occur::Optional,
-        None,
-    );
-
-    args.parse(input)?;
-    let help: Result<bool, ArgsError> = args.value_of("help");
-
-    match help {
-        Ok(is_help) => {
-            if is_help {
-                println!("{}", args.full_usage());
-                return Result::Ok(Action::HELP);
-            }
-        }
-        Err(err) => {
-            panic!("Error: {}", err);
-        }
-    }
-
-    let copy: bool = args.value_of("copy").unwrap_or(false);
-
-    let src: Result<String, ArgsError> = args.value_of("src");
-    let dst: Result<String, ArgsError> = args.value_of("dest");
-    let log: Result<String, ArgsError> = args.value_of("log_file");
-
-    if src.is_err() || dst.is_err() {
-        println!("{}", args.full_usage());
-        return Result::Err(ArgsError::new(
-            "args",
-            error_messages::BOTH_MUST_BE_PROVIDED,
-        ));
-    }
-
-    let result = Config {
-        source: src?.to_string(),
-        destination: dst?.to_string(),
-        logfile: match log {
-            Ok(log) => Option::from(log),
-            Err(_) => Option::None,
-        },
-        dry_run: match args.value_of("dry_run") {
-            Ok(d) => d,
-            Err(_) => false,
-        },
-        copy,
-    };
-
-    // OK it's not help, let' see what we have here.
-
-    return Result::Ok(Action::CONVERT(result));
 }
 
 #[cfg(test)]
