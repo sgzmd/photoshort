1d8b9c852e5329e00574a27675548fec307d1064
Roman Kirillov <sigizmund@gmail.com>
2021-03-14 16:27:50 +0000

    Extracting i-th zip file to a function

--- src/zipfiles.rs
+++ src/zipfiles.rs
@@ -1,13 +1,15 @@
 use crate::config::configurator::Config;
 use crate::discovery::discovery;
 use crate::discovery::discovery::discover_file;
-use crate::pserror::error::PsError;
+use crate::pserror::error::{PsError, PsErrorKind};
 use crate::{move_photo, update_photo_new_path};
+use futures::Future;
 use indicatif::{ProgressBar, ProgressStyle};
 use log::{info, warn};
 use std::fs::File;
 use std::path::Path;
-use tempfile::TempDir;
+use tempfile::{NamedTempFile, TempDir};
+use zip::ZipArchive;
 
 pub fn process_zip_file(file_path: &str, cfg: &Config) -> Result<u64, ()> {
     let file = File::open(Path::new(file_path)).unwrap();
@@ -25,69 +27,17 @@ pub fn process_zip_file(file_path: &str, cfg: &Config) -> Result<u64, ()> {
     let mut num_files_copied = 0;
     let temp_dir = TempDir::new().unwrap();
     let temp_dir_path = temp_dir.path();
+
     for i in 0..zf.len() {
         bar.inc(1);
-        let mut file = zf.by_index(i).unwrap();
-        if file.name().ends_with("/") || !discovery::is_supported_file(file.name()) {
-            // Directory, not interesting
-            continue;
-        }
-
-        let temp_file_path = format!(
-            "{}/{}",
-            temp_dir_path.to_str().unwrap(),
-            file.mangled_name().file_name().unwrap().to_str().unwrap()
-        );
-        let temp_file_path = Path::new(temp_file_path.as_str());
-
-        let mut temp_file = std::fs::File::create(temp_file_path);
-
-        if temp_file.is_err() {
-            warn!(
-                "Couldn't open temp file {} for writing because of {:?}",
-                temp_file_path.to_str().unwrap(),
-                temp_file.err().unwrap()
-            );
-            continue;
-        }
-
-        let mut temp_file = temp_file.unwrap();
-        let written = std::io::copy(&mut file, &mut temp_file).unwrap();
-
-        info!(
-            "Extracted {} -> {}, {} bytes written",
-            file.name(),
-            temp_file_path.to_str().unwrap(),
-            written
-        );
-
-        let photo = discover_file(temp_file_path);
-        let new_path = Option::from(
-            Path::new(file.name())
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap(),
-        );
-        match photo {
-            Ok(mut photo) => {
-                update_photo_new_path(&cfg.destination, &mut photo, new_path);
-                if move_photo(
-                    &photo,
-                    // note that copy/move flag is ignored here as we
-                    // are creating temp file which we move later
-                    true,
-                    cfg.dry_run,
-                )
-                .is_err()
-                {
-                    warn!("Failed to move file to {}", new_path.unwrap());
-                } else {
-                    num_files_copied += 1;
-                }
+        let result = process_ith_file(&cfg, &mut zf, &mut num_files_copied, temp_dir_path, i);
+        match result {
+            Ok(u) => {
+                info!("Processed {}-th file", i);
+                num_files_copied += u as u64;
             }
-            Err(err) => {
-                info!("Couldn't discover file {}: {:?}", file.name(), err);
+            Err(e) => {
+                warn!("Failed to process file {}: {:?}", i, e);
             }
         }
     }
@@ -95,6 +45,75 @@ pub fn process_zip_file(file_path: &str, cfg: &Config) -> Result<u64, ()> {
     bar.finish();
 
     Ok((num_files_copied))
+}
+
+fn process_ith_file(
+    cfg: &&Config,
+    zf: &mut ZipArchive<File>,
+    num_files_copied: &mut u64,
+    temp_dir_path: &Path,
+    i: usize,
+) -> Result<u8, PsError> {
+    let mut file = zf.by_index(i).unwrap();
+    if file.name().ends_with("/") || !discovery::is_supported_file(file.name()) {
+        // Directory, not interesting
+        return Err(PsError::new(
+            PsErrorKind::FileNotSupported,
+            format!("File not supported: {}", file.name()),
+        ));
+    }
+
+    let mut temp_file = NamedTempFile::new();
+
+    if temp_file.is_err() {
+        return Err(PsError::new(
+            PsErrorKind::IoError,
+            format!("IO Error: {:?}", temp_file.err().unwrap()),
+        ));
+    }
+
+    let mut temp_file = temp_file.unwrap();
+    let written = std::io::copy(&mut file, &mut temp_file).unwrap();
+    let temp_file_path = temp_file.path();
+
+    info!(
+        "Extracted {} -> {}, {} bytes written",
+        file.name(),
+        temp_file_path.to_str().unwrap(),
+        written
+    );
+
+    let photo = discover_file(temp_file_path);
+    let new_path = Option::from(
+        Path::new(file.name())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+    );
+    return match photo {
+        Ok(mut photo) => {
+            update_photo_new_path(&cfg.destination, &mut photo, new_path);
+            let move_file_result = move_photo(
+                &photo,
+                // note that copy/move flag is ignored here as we
+                // are creating temp file which we move later
+                false,
+                cfg.dry_run,
+            );
+            match move_file_result {
+                Ok(_) => Ok(1),
+                Err(e) => {
+                    warn!("Failed to move file to {}", new_path.unwrap());
+                    Err(e.into())
+                }
+            }
+        }
+        Err(err) => {
+            info!("Couldn't discover file {}: {:?}", file.name(), err);
+            Err(err)
+        }
+    };
 }
 
 #[cfg(test)]
